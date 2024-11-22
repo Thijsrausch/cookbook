699cd21c4789b923e15aafc9b4159fbc62c88d48
Thijs Rausch <thijsrausch@cloud.com>
2024-11-22 12:09:43 +0100

    fix router

--- src/router.js
+++ src/router.js
@@ -1,14 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './views/HomePage.vue';
-import RecipePage from './views/RecipePage.vue';
 
 const routes = [
     { path: '/', component: HomePage },
-    { path: '/recipe/:id', component: RecipePage, props: true }
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory('/cookbook/'), // Ensure this matches the base path
     routes,
 });
 
