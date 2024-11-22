dd5e517103e80bc53218ef557503ce012ff034ab
Thijs Rausch <thijsrausch@cloud.com>
2024-11-22 15:38:59 +0100

    add simple search

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,18 @@
 <template>
   <v-container>
+    <!-- Search Bar -->
+    <v-text-field
+        v-model="searchTerm"
+        label="Search Recipes"
+        outlined
+        clearable
+        class="mb-4"
+    />
+
+    <!-- Recipes Grid -->
     <v-row>
       <v-col
-          v-for="recipe in recipes"
+          v-for="recipe in filteredRecipes"
           :key="recipe.id"
           cols="12"
           md="4"
@@ -10,8 +20,14 @@
         <RecipeCard :recipe="recipe" />
       </v-col>
     </v-row>
+
+    <!-- No Recipes Found Message -->
+    <div v-if="filteredRecipes.length === 0" class="text-center">
+      <p>No recipes found.</p>
+    </div>
   </v-container>
 </template>
+
 
 <script>
 import recipes from '../data/recipes.json';
@@ -22,7 +38,22 @@ export default {
   data() {
     return {
       recipes,
+      searchTerm: '', // Bind search input
     };
+  },
+  computed: {
+    // Filter recipes based on search term
+    filteredRecipes() {
+      if (!this.searchTerm) {
+        return this.recipes;
+      }
+      const search = this.searchTerm.toLowerCase();
+      return this.recipes.filter(
+          (recipe) =>
+              recipe.name.toLowerCase().includes(search) ||
+              recipe.description.toLowerCase().includes(search)
+      );
+    },
   },
 };
 </script>
