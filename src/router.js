42b828a454acb7d6ac2b6ef7f0f6bf1e52c73878
Thijs Rausch <thijsrausch@cloud.com>
2024-11-22 14:29:52 +0100

    add route for recipe details

--- src/router.js
+++ src/router.js
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from './views/HomePage.vue';
+import RecipePage from './views/RecipePage.vue';
 
 const routes = [
     { path: '/', component: HomePage },
+    { path: '/recipe/:id', component: RecipePage, props: true },
 ];
 
 const router = createRouter({
-    history: createWebHistory('/cookbook/'), // Ensure this matches the base path
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 });
 
